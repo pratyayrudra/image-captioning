130acbd42a77cb068fb6cdbfe8ac900e239a4cf8
Pratyay Rudra <rudrapratyay1999@gmail.com>
2021-02-07 14:13:41 +0530

    image upload and caption added

--- frontend/src/components/DragAndDrop/DragAndDrop.js
+++ frontend/src/components/DragAndDrop/DragAndDrop.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import "./draganddrop.css";
+const axios = require("axios");
 
 const DragAndDrop = () => {
   const [files, setFiles] = useState([]);
+  const [caption, setCaption] = useState("");
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: "image/*",
+    maxFiles: 1,
     onDrop: (acceptedFiles) => {
       setFiles(
         acceptedFiles.map((file) =>
@@ -25,10 +28,27 @@ const DragAndDrop = () => {
         style={{ width: "200px", margin: "10px 0" }}
         alt="preview"
       ></img>
+      <h4>{caption}</h4>
     </div>
   ));
 
-  let uploadFile = () => console.log("upload file");
+  let uploadFile = async () => {
+    console.log(files[0]);
+    let data = new FormData();
+    data.append("image", files[0], files[0].name);
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+    axios
+      .post("http://localhost:8000/api/model", data, config)
+      .then((response) => {
+        console.log(response.data);
+        setCaption(response.data.data.caption);
+      })
+      .catch((error) => {});
+  };
 
   return (
     <>
