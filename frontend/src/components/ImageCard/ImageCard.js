fde3db79e5d742384e0f09313c68c10a11b1af46
aman <amantibrewal310@gmail.com>
2021-02-07 12:08:00 +0530

    flex wrap added in image gallery

--- frontend/src/components/ImageCard/ImageCard.js
+++ frontend/src/components/ImageCard/ImageCard.js
@@ -4,21 +4,19 @@ import './imageCard.css';
 const ImageCard = (props) => {
 	let { imageSrc, caption } = { ...props };
 	return (
-		<div className='wrapper'>
-			<div className='card'>
-				<img
-					src='https://www.pixel4k.com/wp-content/uploads/2019/03/spiderman-miles-lost-in-space-4k_1553071367.jpg'
-					alt='dsfs'
-				/>
-				<div className='info'>
-					<h1>Heading</h1>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing
-						elit. Quam, quidem quis nulla minima inventore cum
-						commodi dolorem autem officiis delectus non maiores quia
-						numquam iure voluptatum et porro iste eaque!
-					</p>
-				</div>
+		<div className='card'>
+			<img
+				src='https://www.pixel4k.com/wp-content/uploads/2019/03/spiderman-miles-lost-in-space-4k_1553071367.jpg'
+				alt='dsfs'
+			/>
+			<div className='info'>
+				<h1>Heading</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					Quam, quidem quis nulla minima inventore cum commodi dolorem
+					autem officiis delectus non maiores quia numquam iure
+					voluptatum et porro iste eaque!
+				</p>
 			</div>
 		</div>
 	);
